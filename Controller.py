7779c2e460f44c24da29287a03a4d51a1b0a2864
Przemek <przemek.popielarski@gmail.com>
2020-03-18 23:24:39 +0100

    Initial commit

---
+++ Controller.py
@@ -0,0 +1,27 @@
+import DataSource
+import itertools
+
+
+def get_common_effects(ingredient1, ingredient2):
+    """Returns set of effects created by combination of two ingredients"""
+    effects_ing1 = set()
+    effects_ing2 = set()
+    for effect in DataSource.effect_to_ingredients_dict:
+        if ingredient1 in DataSource.effect_to_ingredients_dict[effect]:
+            effects_ing1.add(effect)
+        if ingredient2 in DataSource.effect_to_ingredients_dict[effect]:
+            effects_ing2.add(effect)
+    return effects_ing1.intersection(effects_ing2)
+
+
+def get_possible_effects_combinations(selected_ingredients_set):
+    """Returns dict effect -> set of pairs of ingredients (tuples)"""
+    effects_to_ingredients = dict()
+    for pair in itertools.combinations(selected_ingredients_set, 2):
+        effects_list = get_common_effects(pair[0], pair[1])
+        for effect in effects_list:
+            if effect in effects_to_ingredients.keys():
+                effects_to_ingredients[effect].add(pair)
+            else:
+                effects_to_ingredients[effect] = {pair, }
+    return effects_to_ingredients
---
+++ Main.py
@@ -0,0 +1,6 @@
+import DataSource
+from GUI import GUI
+
+if __name__ == "__main__":
+    data_source = DataSource.DataSource()
+    GUI(data_source)
